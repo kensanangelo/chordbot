0f368f5010b6298a6847d4482764b625f2f53ba5
Ken San Angelo <k.sanangelo@knights.ucf.edu>
2015-10-07 19:21:21 -0400

    Added dummy strings to twitKey-template.js to prevent Twit from throwing errors

--- twitKey-template.js
+++ twitKey-template.js
@@ -1,10 +1,10 @@
 var Twit = require('twit');
 
 var keys = new Twit({
-    consumer_key:         '',
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  ''
+    consumer_key:         'xxxx',
+    consumer_secret:      'xxxx',
+    access_token:         'xxxx',
+    access_token_secret:  'xxxx'
 });
 
 exports.twit = keys;
